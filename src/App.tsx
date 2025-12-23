f07e47821fd9abeaef86b28517a4eb8b8e9ea484
QuinnC <cooperstein.quinn@gmail.com>
2025-12-23 12:44:36 -0600

    big

--- src/App.tsx
+++ src/App.tsx
@@ -1,57 +1,254 @@
 "use strict";
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef } from "react";
+
+import gsap from 'gsap';
+import { useGSAP } from '@gsap/react';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import { ScrollSmoother } from "gsap/ScrollSmoother";
+
+gsap.registerPlugin(useGSAP);
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
 
 function App() {
-  const [opacity, setOpacity] = useState(0);
-  const [bottom, setBottom] = useState(24);
-  const [rotation, setRotation] = useState(0);
+  // const [height, setHeight] = useState(window.innerHeight - 40);
+  // const header = useRef<HTMLDivElement>(null);
 
-  // run at start
-  useEffect(() => {
-    const handleScroll = () => {
-      setOpacity(Math.max(0, (275 - scrollY)/275));
-      setBottom(24 + (scrollY) / 275 * 20);
-      setRotation(scrollY / window.innerHeight * 360);
-    };
+  // useEffect(() => {
+  //   setHeight(header.current ? header.current.getBoundingClientRect().height - 16 : 40);
 
-    window.addEventListener("scroll", handleScroll);
+  //   window.addEventListener("resize", () => {
+  //     setHeight(header.current ? header.current.getBoundingClientRect().height - 16 : 40);
+  //   });
+  // }, []);
 
-    setOpacity(Math.max(0, (275 - scrollY)/275));
+  useGSAP(() => {
+    ScrollSmoother.create({
+      smooth: 0.5,
+      smoothTouch: 0,
+      effects: true,
+    })
 
-    // cleanup I guess
-    return () => window.removeEventListener("scroll", handleScroll);
-  }, []);
+    var scrollMoreTL = gsap.timeline({
+      scrollTrigger: {
+        start: 0,
+        end: 250,
+        scrub: true,
+      }
+    })
+
+    scrollMoreTL.to(".scrollMore", {
+      bottom: 75,
+      opacity: 0,
+      display: "none"
+    });
+
+    // make noise noisy
+    gsap.to("#seed", {
+      repeat: -1,
+      repeatDelay: 0.25,
+      duration: 0,
+      ease: "none",
+      onRepeat: () => {
+        gsap.to("#seed", {
+          attr: { seed: Math.floor(Math.random() * 1000) },
+          duration: 0,
+          ease: "none"
+        });
+        console.log("update");
+      }
+    });
+
+    let mm = gsap.matchMedia();
+
+    mm.add("(max-width: 1023px", () => {
+      // set init
+      gsap.set(".innerImage", {
+        clearProps: "all",
+      });
+
+      //   // gsap.set("#name", {
+      //   //   display: "flex",
+      //   //   alignItems: "center",
+      //   //   justifyContent: "center",
+      //   // });
+
+      var tl = gsap.timeline({
+        scrollTrigger: {
+          start: 0,
+          endTrigger: "#text",
+          end: "bottom bottom",
+          scrub: true,
+
+          // pin: ".image",
+          // pinSpacing: false,
+        }
+      });
+
+      tl.from("body", {
+        background: "#fffee6",
+        duration: 0.2,
+      }, 0);
+
+      tl.to({}, { duration: 0 }, 1);
+      //   tl.to(".innerImage", {
+      //     height: "10rem",
+      //     xPercent: -50,
+      //     yPercent: -40,
+      //     duration: 1,
+      //   }, 0);
+
+      //   // tl.to("#name div h1", {
+      //   //   width: "100%",
+      //   //   duration: 0,
+      //   // }, 0.4);
+      //   // tl.to("#name", {
+      //   //   width: "100%",
+      //   //   alignItems: "start",
+      //   //   justifyContent: "start",
+      //   // }, 0.4);
+
+      //   tl.to(".innerImage", {
+      //     opacity: 0,
+      //     duration: 0.3,
+      //   }, 0.4);
+    });
+
+    mm.add("(min-width: 1024px)", () => {
+      gsap.set(".innerImage", {
+        // clearProps: "all", // Remove all GSAP inline styles first
+        xPercent: -50,
+        yPercent: -50,
+        left: "50%",
+        top: "50%",
+        scale: 0.95
+      });
+      gsap.set("#name", { opacity: 1, display: "grid" });
+
+      var tl = gsap.timeline({
+        scrollTrigger: {
+          start: 0,
+          endTrigger: "#text",
+          end: "bottom bottom",
+          scrub: 0.4,
+
+          pin: ".image",
+          pinSpacing: false,
+        }
+      });
+
+      tl.set(".innerImage", {
+        xPercent: 0,
+        scale: 0.95,
+      });
+
+      tl.from("body", {
+        background: "#fffee6",
+        duration: 0.2,
+      }, 0);
+      tl.to("#name", {
+        opacity: 0,
+        display: "none",
+        duration: 0.2,
+      }, 0.1);
+      tl.to(".innerImage", {
+        xPercent: -100,
+        scale: 0.95,
+        duration: 0.5,
+      }, 0);
+      tl.from(".prose", {
+        opacity: 0,
+        duration: 0.4
+      }, 0.1)
+      tl.to({}, { duration: 0 }, 1);
+    });
+  }, { dependencies: [] });
 
   return (
     <>
-      <div className="w-full bg-[#fca8f5] shadow flex flex-row p-2 px-6 justify-between items-center fixed top-0 left-0 right-0 z-50">
+      <div id="name" className="not-lg:hidden fixed w-full h-full grid grid-cols-2">
+        <div className="col-span-1 flex items-center justify-center">
+          <h1 className="w-min text-5xl font-bold z-50">Sarah Engle Counseling</h1>
+        </div>
+      </div>
+      <div className="bg">
+        {/* <svg>
+          <defs>
+            <filter id="nnnoise-filter" x="-20%" y="-20%" width="140%" height="140%" filterUnits="objectBoundingBox" primitiveUnits="userSpaceOnUse" colorInterpolationFilters="linearRGB">
+              <feTurbulence id="seed" type="turbulence" baseFrequency="0.183" numOctaves="4" seed="15" stitchTiles="stitch" x="0%" y="0%" width="100%" height="100%" result="turbulence"></feTurbulence>
+              <feSpecularLighting surfaceScale="12" specularConstant="0.5" specularExponent="20" lightingColor="#7957A8" x="0%" y="0%" width="100%" height="100%" in="turbulence" result="specularLighting">
+                <feDistantLight azimuth="3" elevation="76"></feDistantLight>
+              </feSpecularLighting>
+              <feColorMatrix type="saturate" values="0" x="0%" y="0%" width="100%" height="100%" in="specularLighting" result="colormatrix"></feColorMatrix>
+            </filter>
+          </defs>
+        </svg> */}
+        {/* <svg><defs><filter id="nnnoise-filter" x="-20%" y="-20%" width="140%" height="140%" filterUnits="objectBoundingBox" primitiveUnits="userSpaceOnUse" colorInterpolationFilters="linearRGB">
+          <feTurbulence id="seed" type="turbulence" baseFrequency="0.183" numOctaves="4" seed="15" stitchTiles="stitch" x="0%" y="0%" width="100%" height="100%" result="turbulence"></feTurbulence>
+          <feSpecularLighting surfaceScale="6" specularConstant="0.7" specularExponent="20" lightingColor="#7957A8" x="0%" y="0%" width="100%" height="100%" in="turbulence" result="specularLighting">
+            <feDistantLight azimuth="3" elevation="86"></feDistantLight>
+          </feSpecularLighting>
+          <feColorMatrix type="saturate" values="0" x="0%" y="0%" width="100%" height="100%" in="specularLighting" result="colormatrix"></feColorMatrix>
+        </filter></defs></svg> */}
+        {/* <svg><defs><filter id="nnnoise-filter" x="-20%" y="-20%" width="140%" height="140%" filterUnits="objectBoundingBox" primitiveUnits="userSpaceOnUse" color-interpolation-filters="linearRGB">
+          <feTurbulence id="seed" type="turbulence" baseFrequency="0.183" numOctaves="4" seed="15" stitchTiles="stitch" x="0%" y="0%" width="100%" height="100%" result="turbulence"></feTurbulence>
+          <feSpecularLighting surfaceScale="20" specularConstant="1.5" specularExponent="20" lighting-color="#7957A8" x="0%" y="0%" width="100%" height="100%" in="turbulence" result="specularLighting">
+            <feDistantLight azimuth="3" elevation="86"></feDistantLight>
+          </feSpecularLighting>
+          <feColorMatrix type="saturate" values="0" x="0%" y="0%" width="100%" height="100%" in="specularLighting" result="colormatrix"></feColorMatrix>
+        </filter></defs></svg> */}
+        <svg><defs><filter id="nnnoise-filter" x="-20%" y="-20%" width="140%" height="140%" filterUnits="objectBoundingBox" primitiveUnits="userSpaceOnUse" colorInterpolationFilters="linearRGB">
+          <feTurbulence id="seed" type="turbulence" baseFrequency="0.183" numOctaves="4" seed="15" stitchTiles="stitch" x="0%" y="0%" width="100%" height="100%" result="turbulence"></feTurbulence>
+          <feSpecularLighting surfaceScale="7" specularConstant="1.7" specularExponent="20" lightingColor="#7957A8" x="0%" y="0%" width="100%" height="100%" in="turbulence" result="specularLighting">
+            <feDistantLight azimuth="3" elevation="66"></feDistantLight>
+          </feSpecularLighting>
+          <feColorMatrix type="saturate" values="0" x="0%" y="0%" width="100%" height="100%" in="specularLighting" result="colormatrix"></feColorMatrix>
+          <feComponentTransfer>
+            <feFuncA type="linear" slope="0.3" /> {/* opacity */}
+          </feComponentTransfer>
+        </filter></defs></svg>
+        {/* <svg>
+          <defs>
+            <filter id="nnnoise-filter" x="-20%" y="-20%" width="140%" height="140%" filterUnits="objectBoundingBox" primitiveUnits="userSpaceOnUse" colorInterpolationFilters="linearRGB">
+              <feTurbulence id="seed" type="turbulence" baseFrequency="0.183" numOctaves="4" seed="15" stitchTiles="stitch" x="0%" y="0%" width="100%" height="100%" result="turbulence"></feTurbulence>
+              <feSpecularLighting surfaceScale="6" specularConstant="3" specularExponent="20" lightingColor="#7957A8" x="0%" y="0%" width="100%" height="100%" in="turbulence" result="specularLighting">
+                <feDistantLight azimuth="3" elevation="86"></feDistantLight>
+              </feSpecularLighting>
+              <feColorMatrix type="saturate" values="0" x="0%" y="0%" width="100%" height="100%" in="specularLighting" result="colormatrix"></feColorMatrix>
+            </filter>
+          </defs>
+        </svg> */}
+      </div>
+      {/* <div id="bottomNav" className="fixed bottom-0 left-0 w-dvw z-30 p-1.5 backdrop-blur-md not-lg:hidden"><a href="/">Sarah Engle Counseling</a></div> */}
+      <div id="smooth-wrapper">
+        {/* <div ref={header} className="fixed top-0 left-0 right-0 w-full bg-[#ff72f36a] backdrop-blur-lg backdrop-brightness-150 shadow flex flex-row p-2 px-6 justify-between items-center z-50">
         <a href="/"><span>SE | </span>Sarah Engle Counseling</a>
-        <div>
-          <a href="#about">About</a>
+      </div> */}
+        <div className="not-lg:hidden fixed left-0 right-0 w-full flex justify-center bottom-3 scrollMore z-40">
+          <span className="not-lg:hidden bg-[#fffdff5c] p-2 rounded backdrop-blur-sm backdrop-brightness-200">Scroll for more</span>
+        </div>
+        <div className="flex flex-col" id="smooth-content">
+          <div className="image h-dvh w-full relative overflow-hidden pointer-events-none z-40">
+            <img src="/sarah.png" alt="Sarah Engle" className="innerImage not-lg:w-full customH not-lg:object-contain rounded h-full absolute pointer-events-auto z-40" style={{ left: "50%", top: "50%", transform: "translate(-50%, -50%)", aspectRatio: 618 / 870 }} />
+            <h1 className="lg:hidden absolute bottom-2 w-full text-center text-4xl font-bold">Sarah Engle Counseling</h1>
+          </div>
+          <div id="text" className="w-full flex flex-col items-center mt-8 p-8 lg:grid lg:grid-cols-2">
+            <article className="prose text-justify indent-8 prose-headings:indent-0 lg:col-start-2 lg:pl-4">
+              <h3 id="about">About me:</h3>
+              <p>I graduated from University of Missouri - St. Louis in May of 2024 with my Master of Education in Clinical Mental Health Counseling degree, as well as a certificate in Social Justice & Multicultural Counseling. I have been working at Sonder Counseling since October of 2024. During my time at Sonder Counseling, I have worked with both individuals and couples. In addition to working at Sonder, I also work at CHADS Coalition for Mental Health, doing school-based counseling with middle schoolers and high schoolers. In my free time, I enjoy reading novels and poetry, writing poetry, walking or jogging outside, and cuddling with my cat. I am being supervised for my license by Jackie DeWald. My license number is: 2024046022</p>
+              <hr />
+              <h3>About my practice as a counselor:</h3>
+              <p>Charles Bukowski poses the following question in his novel, Post Office:</p><blockquote>"Who were you, before the world told you who you should be?"</blockquote> <p>In my work as a counselor, my goal is to help each of my clients answer this question. The process of figuring out who we are underneath the masks which the people around us ask us to wear is often a painful, arduous process. However, as a counselor, I am with my clients every step of the way through this process. Together, my clients and I are like fellow archeologists, sifting through the dirt that society layers on top of each of us, so that we can uncover the gems hidden underneath; gems that show my clients who they truly are and what they truly need in their relationships. I focus a lot on relationships with my clients, as I believe that our relationships (which includes the society we live in) is the soil from which our thoughts and feelings grow. In relationship work with my clients, I focus on the following: helping my clients identify and clarify their personal values, boundary-setting, and communication skills.</p>
+              <p>The theory behind my practice is strongly influenced by person-centered therapy, and relational therapies such as Relational-Cultural Therapy. However, I also lean heavily on behavior therapies such as Acceptance and Commitment Therapy and Radically Open Dialectical Behavior Therapy. I have experience working in grief and bereavement, with clients who have experienced traumatic loss, as well as with clients who are living with anxiety, depression, and/or personality disorders. I am open to working with individuals, as well as with partners. I am familiar with and knowledgeable about polyamory, and am comfortable working with multiple partners together in one session. I proudly identify as part of the LGBTQIA+ community and am passionate about working with clients who also identify as being a part of that community. In addition, my practice as a counselor is sex-positive, and weight-inclusive.</p>
+            </article>
+          </div>
+          {/* <div className="min-h-dvh">
+          <div className="prose prose-h3:indent-0 prose-h3:mb-2">
+            <h3>Contact me:</h3>
+            <p>hello</p>
+          </div>
+        </div>  */}
         </div>
       </div>
-      <div className="grid items-center justify-center h-dvh">
-        <div className="fixed left-0 right-0 flex w-full justify-center items-center" style={{opacity: opacity, bottom: bottom}}>Scroll for more</div>
-        <div className="z-30 md:grid w-full md:grid-cols-5 flex flex-col md:flex-col p-6">
-          <div className="md:col-span-2 md:px-6 flex flex-col justify-center top-0 left-0 prose mb-8">
-            <h1 className="text-5xl font-bold mb-3 oswald">Sarah Engle Counseling</h1>
-            <p className="text-lg whitespace-pre-line">hello! I am Sarah Engle!{"\n"}
-              learn about me here congratulations
-            </p>
-          </div>
-          <div className="md:col-span-3 grid items-center justify-center">
-            <img src="lady.png" alt="lady" className="w-full" style={{transform: `rotate(${rotation}deg)`}} />
-          </div>
-        </div>
-      </div>
-      <div className="min-h-dvh pt-16 p-6 flex flex-col items-center justify-center" id="about">
-          <div className="prose">
-            <h1 className="text-3xl font-semibold w-full">About me!</h1>
-            <img className="rounded max-w-full max-h-1/2" src="https://medschool.umich.edu/sites/default/files/styles/jumbo_16_9/public/2023-10/doctoring-course-hero.jpg?h=f0fb51a5&itok=-w_0Fe57" alt="doctoring" />
-            <p>I am a therapist hello yes. This is me. Am I doctor? Or counselor? I don't know. Haha! Hello!</p>
-          </div>
-      </div>
-      <div className="bg-[#fca8f5] w-full p-6 "><p className="prose">hello this is a footer</p></div>
     </>
   )
 }
